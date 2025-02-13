298c4180d1526c7d8c4bc55909f5c9e30efe3990
Cristian Fonseca <cridafoca1@gmail.com>
2025-02-12 22:44:07 -0500

    Jazzmin Settings agregado

---
+++ configuracion/jazzmin_settings.py
@@ -0,0 +1,9 @@
+JAZZMIN_SETTINGS = {
+    "site_title": "MedRecord",
+    "site_header": "MedRecord",
+    "site_brand": "MedRecord",
+    "welcome_sign": "Bienvenido a MedRecord",
+    "copyright": "MedRecord",
+    "search_model": "auth.User",
+    "user_avatar": None,
+}
